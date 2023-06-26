fa72c11f8ca9a96e6166690e8ea514b457a46357
Mark Shaffer <31354065+CodeMelted@users.noreply.github.com>
2023-06-25 21:24:07 -0400

    Put back to just generating the README.md since all other socials under main project.

--- build.ps1
+++ build.ps1
@@ -24,33 +24,16 @@
 # =============================================================================
 
 [string]$HTML_TEMPLATE = @'
-<!DOCTYPE html>
-<html lang="en"><head>
-    <title>Melt the Code - GitHub</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="Mark Shaffer">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="monetization" content="$ilp.uphold.com/q94gJPq8PFF4">
-    <link rel="stylesheet" href="https://www.codemelted.com/css/hacker-theme.css">
-    <style>
-        .content-main {
-            background-color: #1C2834;
-        }
-    </style>
-</head><body><div class="content-main">
-    <a class="github-button" href="https://github.com/codemelted" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Follow @codemelted on GitHub">Follow @codemelted</a><br /><br />
-    CONTENT
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-</div></body></html>
+<a class="github-button" href="https://github.com/codemelted" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Follow @codemelted on GitHub">Follow @codemelted</a><br /><br />
+CONTENT
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 '@
 
 function build {
     # -------------------------------------------------------------------------
     # Constants:
     # -------------------------------------------------------------------------
-    [string]$PROJ_NAME = "melt_the_code Socials Generator"
+    [string]$PROJ_NAME = "melt_the_code GitHub Profile Generator"
     [string]$SCRIPT_PATH = $PSScriptRoot
 
     # -------------------------------------------------------------------------
@@ -64,34 +47,10 @@ function build {
 
     # Setup the dist/socials directory
     message $PROJ_NAME
-    message "Clear the dist directory"
-    Remove-Item -Path "dist" -Force -Recurse -ErrorAction Ignore
-    New-Item -Path "dist/socials" -ItemType Directory
-
-    # Go parse the src/ to the dist/ directory
-    Copy-Item -Path "src/podcast.html" -Destination "dist/socials"
-    Copy-Item -Path "src/twitter.html" -Destination "dist/socials"
-    Copy-Item -Path "src/youtube.html" -Destination "dist/socials"
-
-    # Collect our photos information for the src/photography.html
-    message "Now building photo album dataset"
-    $data = @{}
-    $photoPaths = Get-ChildItem -Path "src/photos" -Directory
-    foreach ($photoPath in $photoPaths) {
-        $photoFiles = Get-ChildItem -Path $photoPath.FullName
-            | Foreach-Object {$_.Name}
-        $key = "photos/" + $photoPath.Name
-        $data[$key] = $photoFiles
-    }
-    $jsonData = $data | ConvertTo-Json
-    [string] $photographyHtml = [IO.File]::ReadAllText("src/photography.html")
-    $photographyHtml = $photographyHtml.Replace("const photoData = {};", "const photoData = $jsonData;")
-    $photographyHtml | Out-File "dist/socials/photography.html" -Encoding utf8 -Force
-    Copy-Item -Path "src/photos/" -Destination "dist/socials/" -Recurse -Force
 
     $mdFile = ConvertFrom-Markdown -Path "$SCRIPT_PATH/README.md"
     $htmlData = $HTML_TEMPLATE.Replace("CONTENT", $mdFile.Html)
-    $HtmlData | Out-File -Encoding utf8 "dist/socials/github.html"
+    $HtmlData | Out-File -Encoding utf8 "README.html"
 
     message "$PROJ_NAME processing completed."
 }
